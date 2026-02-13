5d6ea01b22faeb691c6cc59d8f52c2c741a80dd3
vaibhav kumar singh <singh1326vaibhav@gmail.com>
2026-02-14 00:36:54 +0530

    update-profile controller

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoute from './routes/auth.routes.js'
+import cookieParser from "cookie-parser"
 import { connectdb } from "./lib/db.js"
 
 dotenv.config()
@@ -9,7 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(express.json())
-
+app.use(cookieParser())
 app.use('/api/auth',authRoute)
 
 
